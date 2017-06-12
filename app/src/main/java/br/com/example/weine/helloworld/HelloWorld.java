148cda8b4da1abb18a4aff9c6d8a25cb79030204
Weine Duarte <weineduarte@gmail.com>
2017-06-11 21:15:25 -0300

    imagem menu

--- app/src/main/java/br/com/example/weine/helloworld/HelloWorld.java
+++ app/src/main/java/br/com/example/weine/helloworld/HelloWorld.java
@@ -2,12 +2,35 @@ package br.com.example.weine.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class HelloWorld extends AppCompatActivity {
-
+    private TextView tviTeste;
+    private Button btOk;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_world);
+        findViews();
+        setAction();
+    }
+
+    private void findViews() {
+        tviTeste = (TextView)findViewById(R.id.tvi_teste);
+        btOk = (Button) findViewById(R.id.bt_ok);
+    }
+
+    private void setAction(){
+        btOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tviTeste.setText("coisa qualquer");
+            }
+        });
+
+
+
     }
 }
